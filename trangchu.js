85eb33e939334dcfc2762b369fcef3928668ef25
minhtan39 <161004292+minhtan39@users.noreply.github.com>
2024-08-09 06:30:20 +0700

    Update trangchu.js

--- trangchu.js
+++ trangchu.js
@@ -4,3 +4,14 @@ function toggleSidebar() {
     const isOpen = sidebar.classList.toggle('open');
     menuButton.classList.toggle('open', isOpen);
 }
+/*Chạm vào bất kỳ Sidebar thu vào*/
+document.addEventListener('click', (event) => {
+    const sidebar = document.querySelector('.sidebar');
+    const menuButton = document.querySelector('.menu-button');
+
+    // Kiểm tra nếu nhấp ra ngoài sidebar và menu button
+    if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+        sidebar.classList.remove('open');
+        menuButton.classList.remove('open');
+    }
+});
